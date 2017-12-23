2aed45abcf38a880947b77dc04621b04a00c0816
Ashwin George Philip <ashwinphilip27@gmail.com>
2017-12-23 04:33:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Ashwin"
 var height = 159
-var message = ` is ${height} centimeters tall`
+var message = ` ${name} is ${height} centimeters tall`
 
 module.exports = {name, height, message}
